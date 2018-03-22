f3611e303a0b08016a3b43804b5d65d0a2aaee4e
Nurym Kenzh <nak290284@gmail.com>
2018-03-22 16:01:15 +0600

    Init

---
+++ GeoServer/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace GeoServer.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
