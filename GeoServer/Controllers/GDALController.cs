7cf0254d8d2468251a54e54a380bef4661a0e61e
Nurym Kenzh <nak290284@gmail.com>
2018-03-24 00:06:08 +0600

    GDAL.GetRasterBandsCount

--- GeoServer/Controllers/GDALController.cs
+++ GeoServer/Controllers/GDALController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
@@ -18,7 +19,7 @@ namespace GeoServer.Controllers
             _hostingEnvironment = hostingEnvironment;
         }
 
-        private string PythonExecute(params string[] Arguments)
+        private string PythonExecuteWithParameters(params string[] Arguments)
         {
             Process process = new Process();
             try
@@ -40,7 +41,49 @@ namespace GeoServer.Controllers
                 string pyhonOutput = process.StandardOutput.ReadToEnd();
                 string pyhonError = process.StandardError.ReadToEnd();
                 process.WaitForExit();
-                if(!string.IsNullOrEmpty(pyhonError))
+                if (!string.IsNullOrEmpty(pyhonError))
+                {
+                    throw new Exception(pyhonError);
+                }
+                else
+                {
+                    return pyhonOutput;
+                }
+            }
+            catch (Exception exception)
+            {
+                throw new Exception(exception.ToString(), exception.InnerException);
+            }
+        }
+
+        private string PythonExecute(params string[] Arguments)
+        {
+            Process process = new Process();
+            try
+            {
+                Arguments[0] = Path.GetFullPath(
+                    Path.ChangeExtension(
+                        Path.Combine(
+                            _hostingEnvironment.ContentRootPath,
+                            Path.Combine("Python", Arguments[0])),
+                        "py")
+                    );
+
+                process.StartInfo.UseShellExecute = false;
+                process.StartInfo.RedirectStandardOutput = true;
+                process.StartInfo.RedirectStandardInput= true;
+                process.StartInfo.RedirectStandardError = true;
+                process.StartInfo.FileName = Startup.Configuration["GDAL:PythonFullPath"];
+                process.StartInfo.Arguments = Arguments[0];
+                process.Start();
+                for(int i = 1; i < Arguments.Count(); i++)
+                {
+                    process.StandardInput.WriteLine(Arguments[i]);
+                }
+                string pyhonOutput = process.StandardOutput.ReadToEnd();
+                string pyhonError = process.StandardError.ReadToEnd();
+                process.WaitForExit();
+                if (!string.IsNullOrEmpty(pyhonError))
                 {
                     throw new Exception(pyhonError);
                 }
--- GeoServer/Python/GetRasterBandsCount.py
+++ GeoServer/Python/GetRasterBandsCount.py
@@ -1,9 +1,7 @@
 try:
-        import sys
         import gdal
-        if __name__ == '__main__':
-                file = sys.argv[1]
-                ds = gdal.Open(file)
-                print ds.RasterCount
+        file = raw_input()
+        ds = gdal.Open(file)
+        print ds.RasterCount
 except Exception as exception:
         raise ValueError(exception)
