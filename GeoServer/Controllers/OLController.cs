04d6c93fd9a19f913e8fc12590d58b335ed9d97a
Нурым Кенжебеков <nkenzhebekov@ecoservice.kz>
2018-07-10 13:52:12 +0600

    OL:ViewModis

--- GeoServer/Controllers/OLController.cs
+++ GeoServer/Controllers/OLController.cs
@@ -2,14 +2,28 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using GeoServer.Data;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace GeoServer.Controllers
 {
     public class OLController : Controller
     {
+        private readonly ApplicationDbContext _context;
+
+        public OLController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
         public IActionResult ViewModis()
         {
+            var modisSources = _context.ModisSource.OrderBy(m => m.Name);
+            ViewBag.ModisSource = new SelectList(modisSources, "Name", "Name");
+            var modisProducts = _context.ModisProduct.Where(m => m.ModisSourceId == _context.ModisSource.OrderBy(ms => ms.Name).FirstOrDefault().Id).OrderBy(m => m.Name);
+            ViewBag.ModisProduct = new SelectList(modisProducts, "Name", "Name");
+            ViewBag.ModisDataSet = new SelectList(_context.ModisDataSet.Where(m => m.ModisProductId == modisProducts.FirstOrDefault().Id).OrderBy(m => m.Index), "Name", "IndexName");
             return View();
         }
     }
