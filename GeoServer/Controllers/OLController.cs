04fb2d962107c6fc67db1fa411437624ad27b8e5
Нурым Кенжебеков <nkenzhebekov@ecoservice.kz>
2018-07-10 09:47:32 +0600

    OL

---
+++ GeoServer/Controllers/OLController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GeoServer.Controllers
+{
+    public class OLController : Controller
+    {
+        public IActionResult ViewModis()
+        {
+            return View();
+        }
+    }
+}
